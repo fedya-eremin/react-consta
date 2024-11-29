392d9786e3988cd8b2f29e8ed2962d7b2596ea57
fedya-eremin <qwertier06@gmail.com>
2024-11-29 13:38:39 +0300

    feat: add auth

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import {Button} from "@consta/uikit/Button"
+import {User} from "@consta/uikit/User"
+import { useNavigate } from "react-router-dom"
+
+const Header = () => {
+  const navigate = useNavigate();
+  return (
+    <header style={{
+      display: "flex",
+      justifyContent: "space-evenly",
+      position: "sticky",
+      top: 0,
+      paddingBottom: "20px",
+    }}>
+      <div>
+        <Button label="Главная страница" onClick={() => navigate("/")} />
+        <Button label="Услуги компании" onClick={() => navigate("/about")} />
+      </div>
+      <div>
+        <User />
+        <Button label="Вход" onClick={() => navigate("/sign_in")}/>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
