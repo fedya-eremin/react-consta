ef9569f3272f1d00e37d12b51356fd2d7fcec27a
fedya-eremin <qwertier06@gmail.com>
2024-12-01 15:52:34 +0300

    feature: созданы услуги и апи к ним

---
+++ src/pages/services/Service.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import MyCard from '../../components/card/Card'; 
+import Header from '../../components/header/Header';
+
+const ServiceDetail = () => {
+  const { id } = useParams();
+  const [service, setService] = useState();
+
+  useEffect(() => {
+    fetch(`https://673423afa042ab85d1190055.mockapi.io/api/v1/services/${id}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setService(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching the service:', error);
+      });
+  }, [id]);
+
+  return (
+    <>
+      <Header />
+      {service ? (
+        <MyCard 
+          imgURI={service.image} 
+          name={service.name} 
+          desc={service.description} 
+          id={service.id} 
+        />
+      ) : (
+        <div>Услуга не найдена.</div>
+      )}
+    </>
+  );
+};
+
+export default ServiceDetail;
+
